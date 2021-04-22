7859328164caedc0e506141bb347fc24b87d132b
moonlight <moonlight@gmail.com>
2021-04-22 22:09:01 +0800

    'first'

---
+++ form.php
@@ -0,0 +1,15 @@
+<?php
+
+$formiin_medeelel =$_POST;
+$email_hayag =$formiin_medeelel["email"];
+
+print_r($email_hayag);
+echo "<hr>";
+print_r($formiin_medeelel);
+
+// [
+// 'email'=> ldkfkj
+// 'name' =>  lkfdgj
+// 'message' =>  ;fdojg )
+// ]
+?>
