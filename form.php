15269217b65014edcc420e36475c1cc8a5832043
moonlight <moonlight@gmail.com>
2021-04-23 17:15:58 +0800

    anhnii tvlhelt

--- form.php
+++ form.php
@@ -1,15 +1,63 @@
 <?php
 
-$formiin_medeelel =$_POST;
-$email_hayag =$formiin_medeelel["email"];
 
-print_r($email_hayag);
+
+$x = 10;
+$y = "10";
+echo $x != $y;
+
+
 echo "<hr>";
-print_r($formiin_medeelel);
+
+$email = $_POST['email'];
+$name = $_POST['name'];
+$message = $_POST['message'];
+
+
+$oguulber = "hey sorry";
+
+echo ($email);
+echo strlen($email);
+
+die();
+
+if(strlen($email) == "") {
+    echo "Email hayag hooson baina";
+    echo "<br>";
+    die();
+} else if($name == "") {
+    echo "ner hooson bna";
+    echo "<br>";
+    die();
+} else if($message == ""){
+    echo "message hooson bna";
+    echo "<br>";
+    die();
+}
+
+
+
+
+
+
+
+
+// $formiin_medeelel =$_POST;
+// $email_hayag =$formiin_medeelel["email"];
+
+// print_r($email_hayag);
+// echo "<hr>";
+// print_r($formiin_medeelel);
 
 // [
 // 'email'=> ldkfkj
 // 'name' =>  lkfdgj
 // 'message' =>  ;fdojg )
-// ]
+// 
 ?>
+<h1>
+<?php
+
+?>
+
+</h1>
