133e9dd5282a72cd8c0331935a31129d166f5745
moonlight <moonlight@gmail.com>
2021-04-24 16:11:13 +0800

    anhnii tvlhelt

--- form.php
+++ form.php
@@ -2,38 +2,54 @@
 
 
 
-$x = 10;
-$y = "10";
-echo $x != $y;
+// $x = 10;
+// $y = "10";
+// echo $x != $y;
 
 
-echo "<hr>";
+// echo "<hr>";
 
-$email = $_POST['email'];
-$name = $_POST['name'];
-$message = $_POST['message'];
+// $email = $_POST['email'];
+// $name = $_POST['name'];
+// $message = $_POST['message'];
 
 
-$oguulber = "hey sorry";
+// $oguulber = "hey sorry";
 
-echo ($email);
-echo strlen($email);
 
-die();
 
-if(strlen($email) == "") {
-    echo "Email hayag hooson baina";
-    echo "<br>";
-    die();
-} else if($name == "") {
-    echo "ner hooson bna";
-    echo "<br>";
-    die();
-} else if($message == ""){
-    echo "message hooson bna";
-    echo "<br>";
-    die();
-}
+switch ($email) {
+    case "moonlight@gmail.com":
+      echo "Apprentice Gmail bainaa";
+      break;
+    case "i@apprentice.mn":
+      echo "moonlight.mn email bainaa";
+      break;
+    case "":
+      echo "Email xayag xooson baina";
+      break;
+    default:
+      echo "emailiing shalgalt duuslaa";
+  }
+
+//   echo ($email);
+// echo strlen($email);
+
+// die();
+
+// if(strlen($email) == "") {
+//     echo "Email hayag hooson baina";
+//     echo "<br>";
+//     die();
+// } else if($name == "") {
+//     echo "ner hooson bna";
+//     echo "<br>";
+//     die();
+// } else if($message == ""){
+//     echo "message hooson bna";
+//     echo "<br>";
+//     die();
+// }
 
 
 
